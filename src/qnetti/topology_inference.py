b1b298173d88045e58b76f9859e43c1f2d2763d7
Daniel Chen <dannychen0830@gmail.com>
2023-01-25 08:53:42 -0500

    added doc strings and tests for topology inference using entropic quantites

--- src/qnetti/topology_inference.py
+++ src/qnetti/topology_inference.py
@@ -1,16 +1,46 @@
-import qnetvo as qnet
+import qnetvo
 import pennylane as qml
 import pennylane.numpy as np
 
 
 def measured_mutual_info_cost_fn(ansatz, **qnode_kwargs):
+    """
+    Constructs an ansatz-specific measured mutual information cost function.
+
+    In the context of quantum networks, the measured mutual information seeks to quantify the correlation between
+    measurement statistics of a pair of measurement devices, where measurements are performed locally on each
+    device.
+
+    Formally, let :math:`X` and :math:`Y` be random variables representing measurement outcomes of two measurement
+    devices in the network, where projective measurements :math:`\\{\\Pi^X\\}` and :math:`\\{\\Pi^Y\\}` are
+    performed on respective devices. Then, the measured mutual information seeks to find the measurement bases
+    that maximizes the mutual information between :math:`X` and :math:`Y`:
+
+    .. math::
+
+            I_m (X;Y) = \\max_{\\{\\Pi^X\\}, \\{\\Pi^X\\}} H(X) + H(Y) - H(XY)
+
+    where :math:`H(\cdot)` denotes the Shannon entropy.
+
+    :param ansatz: The ansatz circuit on which the measured mutual information is evaluated. The ansatz is limited
+                   to having two measurement nodes.
+    :type ansatz: qnetvo.Network Ansatz
+
+    :param qnode_kwargs: Keyword arguments passed to execute qnodes.
+    :type qnode_kwargs: dictionary
+
+    :return: A cost function ``measured_mutual_info_cost(*network_settings)`` parameterized by the ansatz-specific
+             scenario settings.
+    :rtype: Function
+
+    """
     num_prep_nodes = len(ansatz.prepare_nodes)
     num_meas_nodes = len(ansatz.measure_nodes)
     num_qubits = len(ansatz.measure_wires)
     X_num_qubits = len(ansatz.measure_nodes[0].wires)
     Y_num_qubits = len(ansatz.measure_nodes[1].wires)
 
-    probs_qnode = qnet.joint_probs_qnode(ansatz, **qnode_kwargs)
+    probs_qnode = qnetvo.joint_probs_qnode(ansatz, **qnode_kwargs)
 
     def cost(*network_settings):
         settings = ansatz.qnode_settings(
@@ -19,24 +49,30 @@ def measured_mutual_info_cost_fn(ansatz, **qnode_kwargs):
         probs_vec = probs_qnode(settings)
         probs_tensor = probs_vec.reshape((2,) * (X_num_qubits + Y_num_qubits))
 
-        HX = qnet.shannon_entropy(
+        HX = qnetvo.shannon_entropy(
             np.sum(probs_tensor, axis=tuple(range(X_num_qubits, num_qubits))).reshape(
                 (2 * X_num_qubits),
             )
         )
-        HY = qnet.shannon_entropy(
+        HY = qnetvo.shannon_entropy(
             np.sum(probs_tensor, axis=tuple(range(X_num_qubits))).reshape((2 * Y_num_qubits,))
         )
 
-        HXY = qnet.shannon_entropy(probs_vec)
+        HXY = qnetvo.shannon_entropy(probs_vec)
 
         return -(HX + HY - HXY)
 
     return cost
 
 
-def qubit_characteristic_matrix(prepare_node, step_size=0.05, num_steps=101, shot=0):
+def qubit_characteristic_matrix(prepare_node, step_size=0.05, num_steps=501, shots=None):
     """Obtains the qubit characteristic matrix for a given multi-qubit state preparation.
+
+    Mathematically, the qubit characteristic matrix is a real-valued matrix :math:`Q \\in \\mathbb{R}^{n \times n}`,
+    :math:`n` being the number of qubits in a network. On the diagonal, :math:`Q` stores the von Neumann entropy of
+    the respective qubit, i.e. for any :math:`i \\in [n]`, :math:`Q_{ii} = S(q_i)`. On the other hand, off-diagonal
+    entries stores the measured mutual information between qubits: :math:`Q_{ij} = I_m(q_i;q_j)` for :math:`i \neq j`.
+    For further details, see https://arxiv.org/abs/2212.07987.
 
     :param prepare_node: A state preparation for a multi-qubit state
     :type prepare_node: qnetvo.PrepareNode
@@ -44,32 +80,32 @@ def qubit_characteristic_matrix(prepare_node, step_size=0.05, num_steps=101, sho
     :param step_size: The magnitude of each step during optimization
     :type step_size: float
 
-    :param num_step: The number of iterations to perform the optimization
+    :param num_steps: The number of iterations to perform the optimization
     :type num_steps: int
 
-    :param shot: the number of measurement taken on the quantum state
-    :type shot: int
+    :param shots: the number of measurement evaluated on the quantum circuit
+    :type shots: int
 
     :returns: the calculated/measured characteristic matrix of input network
     :rtype: matrix array
     """
 
-    num_qubit = max([qubit for qubit in prepare_node.wires]) + 1
-    if shot > 0:
-        device = {"name": "default.qubit", "shots": 10}
-    else:
+    num_qubits = max([qubit for qubit in prepare_node.wires]) + 1
+    if shots is None:
         device = {}
+    else:
+        device = {"name": "default.qubit", "shots": shots}
 
-    characteristic_matrix = np.zeros(shape=[num_qubit, num_qubit])
+    characteristic_matrix = np.zeros(shape=[num_qubits, num_qubits])
     # compute von Neumann entropies
-    for qubit in range(num_qubit):
+    for qubit in range(num_qubits):
         meas_node = [
-            qnet.MeasureNode(1, 1, wires=[qubit], quantum_fn=qml.ArbitraryUnitary, num_settings=3)
+            qnetvo.MeasureNode(1, 1, wires=[qubit], quantum_fn=qml.ArbitraryUnitary, num_settings=3)
         ]
-        ansatz = qnet.NetworkAnsatz([prepare_node], meas_node, dev_kwargs=device)
-        cost = qnet.shannon_entropy_cost_fn(ansatz)
+        ansatz = qnetvo.NetworkAnsatz([prepare_node], meas_node, dev_kwargs=device)
+        cost = qnetvo.shannon_entropy_cost_fn(ansatz)
         settings = ansatz.rand_network_settings()
-        dict = qnet.gradient_descent(
+        dict = qnetvo.gradient_descent(
             cost,
             settings,
             step_size=step_size,
@@ -80,20 +116,20 @@ def qubit_characteristic_matrix(prepare_node, step_size=0.05, num_steps=101, sho
         characteristic_matrix[qubit, qubit] = -dict["scores"][-1]
 
     # compute meausured mutual information
-    for qubit_1 in range(num_qubit):
-        for qubit_2 in range(qubit_1 + 1, num_qubit):
+    for qubit_1 in range(num_qubits):
+        for qubit_2 in range(qubit_1 + 1, num_qubits):
             meas_node = [
-                qnet.MeasureNode(
+                qnetvo.MeasureNode(
                     1, 1, wires=[qubit_1], quantum_fn=qml.ArbitraryUnitary, num_settings=3
                 ),
-                qnet.MeasureNode(
+                qnetvo.MeasureNode(
                     1, 1, wires=[qubit_2], quantum_fn=qml.ArbitraryUnitary, num_settings=3
                 ),
             ]
-            ansatz = qnet.NetworkAnsatz([prepare_node], meas_node)
+            ansatz = qnetvo.NetworkAnsatz([prepare_node], meas_node)
             cost = measured_mutual_info_cost_fn(ansatz)
             settings = ansatz.rand_network_settings()
-            dict = qnet.gradient_descent(
+            dict = qnetvo.gradient_descent(
                 cost,
                 settings,
                 step_size=step_size,
@@ -108,7 +144,10 @@ def qubit_characteristic_matrix(prepare_node, step_size=0.05, num_steps=101, sho
 
 
 def characteristic_matrix_decoder(characteristic_matrix, tol=1e-5):
-    """Decode the qubit characteristic matrix by grouping indices that have the same rows
+    """Decode the qubit characteristic matrix and partition qubits into their respective preparation nodes.
+
+    If two qubits come from the same preparation node, they are correlated and have identical rows in the qubit
+    characteristic matrix.
 
     :param characteristic_matrix: the qubit characteristic matrix of an unknown network
     :type characteristic_matrix: matrix array
@@ -120,8 +159,11 @@ def characteristic_matrix_decoder(characteristic_matrix, tol=1e-5):
     :rtype: list
     """
 
-    dict = {}
-    num_qubit = characteristic_matrix.shape[0]
+    num_qubits = characteristic_matrix.shape[0]
+
+    # network is a dictionary with prep. nodes as keys,
+    # and the list of qubits that belong to the respective prep node as values
+    network = {}
 
     # convert characteristic matrix to binary (zero/non-zero)
     characteristic_matrix = np.where(characteristic_matrix > tol, 1, 0)
@@ -133,13 +175,13 @@ def characteristic_matrix_decoder(characteristic_matrix, tol=1e-5):
             string += str(arr[i])
         return string
 
-    for row in range(num_qubit):
-        key = array_to_string(characteristic_matrix[row, :])
-        if key in dict:
-            val = dict[key]
-            val.append(row)
-            dict[key] = val
+    for qubit in range(num_qubits):
+        prep_node = array_to_string(characteristic_matrix[qubit, :])
+        if prep_node in network:
+            qubit_list = network[prep_node]
+            qubit_list.append(qubit)
+            network[prep_node] = qubit_list
         else:
-            dict[key] = [row]
+            network[prep_node] = [qubit]
 
-    return [dict[key] for key in dict]
+    return [network[prep_node] for prep_node in network]
--- test/topology_inference_test.py
+++ test/topology_inference_test.py
@@ -1,18 +1,102 @@
 import pytest
 import qnetvo
 import pennylane as qml
-
+import pennylane.numpy as np
 import qnetti
 
 
-def test_qubit_characteristic_matrix():
-    def state_prep(wires):
-        qml.Hadamard(wires[0])
-        qml.CNOT(wires[0:2])
+def test_measured_mutual_info_cost_fn():
+    def state_prep(settings, wires):
+        qml.Hadamard(wires=wires[0])
+        qml.CNOT(wires=[wires[0], wires[1]])
+        qml.Hadamard(wires=[wires[2]])
+        qml.CNOT(wires=[wires[2], wires[3]])
 
-        qml.Hadamard(wires[2])
+    prep_node = qnetvo.PrepareNode(
+        num_in=1, wires=[0, 1, 2, 3], quantum_fn=state_prep, num_settings=0
+    )
+
+    # test entangled qubits
+    meas_node = [
+        qnetvo.MeasureNode(
+            num_in=1, num_out=1, wires=[0], quantum_fn=qml.ArbitraryUnitary, num_settings=3
+        ),
+        qnetvo.MeasureNode(
+            num_in=1, num_out=1, wires=[1], quantum_fn=qml.ArbitraryUnitary, num_settings=3
+        ),
+    ]
+    cost_fn = qnetti.measured_mutual_info_cost_fn(qnetvo.NetworkAnsatz([prep_node], meas_node))
+    assert abs(cost_fn(*(0, 0, 0, 0, np.pi / 2, 0)) - 0) < 1e-9
+    assert abs(cost_fn(*(0, 0, 0, 0, np.pi, np.pi)) - -1) < 1e-9
+    assert abs(cost_fn(*(0, np.pi / 2, np.pi / 2, 0, np.pi / 2, np.pi / 2)) - -1) < 1e-9
+
+    # test unentangled qubits
+    meas_node = [
+        qnetvo.MeasureNode(
+            num_in=1, num_out=1, wires=[1], quantum_fn=qml.ArbitraryUnitary, num_settings=3
+        ),
+        qnetvo.MeasureNode(
+            num_in=1, num_out=1, wires=[2], quantum_fn=qml.ArbitraryUnitary, num_settings=3
+        ),
+    ]
+    cost_fn = qnetti.measured_mutual_info_cost_fn(qnetvo.NetworkAnsatz([prep_node], meas_node))
+    assert abs(cost_fn(*(0, 0, 0, 0, np.pi / 2, 0)) - 0) < 1e-9
+    assert abs(cost_fn(*(0, 0, 0, 0, np.pi, np.pi)) - 0) < 1e-9
+    assert abs(cost_fn(*(0, np.pi / 2, np.pi / 2, 0, np.pi / 2, np.pi / 2)) - 0) < 1e-9
+
+
+def test_qubit_characteristic_matrix():
+    np.random.seed(102)
+    # test if no entanglement are present
+    def state_prep(settings, wires):
+        qml.Hadamard(wires=wires[0])
+        qml.Hadamard(wires=wires[1])
+        qml.Hadamard(wires=wires[2])
 
     prep_node = qnetvo.PrepareNode(num_in=1, wires=[0, 1, 2], quantum_fn=state_prep, num_settings=0)
+    assert np.allclose(
+        qnetti.qubit_characteristic_matrix(prep_node, num_steps=50), np.zeros(shape=(3, 3))
+    )
 
-    # TODO: update tests
-    assert qnetti.qubit_characteristic_matrix(prep_node)
+    np.random.seed(151)
+    # test for one entanglement source
+    def state_prep(settings, wires):
+        qml.Hadamard(wires=wires[0])
+        qml.CNOT(wires=[wires[0], wires[1]])
+        qml.CNOT(wires=[wires[1], wires[2]])
+
+    prep_node = qnetvo.PrepareNode(num_in=1, wires=[0, 1, 2], quantum_fn=state_prep, num_settings=0)
+    assert np.allclose(
+        qnetti.qubit_characteristic_matrix(prep_node, step_size=0.1, num_steps=50),
+        np.ones(shape=(3, 3)),
+    )
+
+    # test for two preparation nodes
+    def state_prep(settings, wires):
+        qml.Hadamard(wires=wires[0])
+        qml.CNOT(wires=[wires[0], wires[1]])
+        qml.Hadamard(wires=[wires[2]])
+        qml.CNOT(wires=[wires[2], wires[3]])
+
+    prep_node = qnetvo.PrepareNode(
+        num_in=1, wires=[0, 1, 2, 3], quantum_fn=state_prep, num_settings=0
+    )
+    assert np.allclose(
+        qnetti.qubit_characteristic_matrix(prep_node, step_size=0.05, num_steps=50),
+        np.kron(np.eye(2), np.ones(shape=(2, 2))),
+    )
+
+
+def test_characteristic_matrix_decoder():
+    # test one prep node
+    assert qnetti.characteristic_matrix_decoder(np.zeros(shape=(3, 3))) == [[0, 1, 2]]
+    assert qnetti.characteristic_matrix_decoder(np.ones(shape=(3, 3))) == [[0, 1, 2]]
+
+    # test for multiple prep node
+    assert qnetti.characteristic_matrix_decoder(np.kron(np.eye(2), np.ones(shape=(2, 2)))) == [
+        [0, 1],
+        [2, 3],
+    ]
+    assert qnetti.characteristic_matrix_decoder(
+        np.array([[1, 1, 0, 0], [0, 0, 1, 1], [1, 1, 0, 0], [0, 0, 1, 1]])
+    ) == [[0, 2], [1, 3]]
